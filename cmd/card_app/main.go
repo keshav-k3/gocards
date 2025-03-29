a527676e253d7a2d737fc9170143635c10298e7a
Keshav Kk <keshavkk.musafir@gmail.com>
2025-03-29 16:15:44 +0900

    init

---
+++ cmd/card_app/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+
+	"github.com/pocketbase/pocketbase"
+)
+
+func main() {
+	app := pocketbase.New()
+
+	err := app.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
